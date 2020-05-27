05286c56029bd944fc29a1d64afb49aea59c4ad9
mekty2012 <41375658+mekty2012@users.noreply.github.com>
2020-05-27 15:47:39 +0900

    Add bytearray example

--- anglican-user/worksheets/BatchReader.clj
+++ anglican-user/worksheets/BatchReader.clj
@@ -16,11 +16,15 @@
 (use 'nstools.ns)
 (require 'mikera.image.core)
 (require 'mikera.image.colours)
+(require 'byte-streams)
 (ns+ template
   (:like anglican-user.worksheet))
 ;; @@
 
 ;; @@
+; This implementation is wrong. We need to modify definition of firstarray so that it uses byte array.
+; Current implementation is using char array, where 1 char may not be single byte.
+
 (defn Example []
   (with-open [rdr (clojure.java.io/reader "worksheets/CS423_Group4/cifar-10-batches-bin/data_batch_1.bin")]
     (let [firstarray (take-nth 3072 (next (mapcat seq (line-seq rdr))))
@@ -43,9 +47,6 @@
 ;; @@
 (def image (mikera.image.core/new-image 32 32))
 ;; @@
-;; =>
-;;; {"type":"html","content":"<span class='clj-var'>#&#x27;template/image</span>","value":"#'template/image"}
-;; <=
 
 ;; @@
 (loop [i 0 j 0]
@@ -63,9 +64,16 @@
 
 (mikera.image.core/show image)
 ;; @@
-;; =>
-;;; {"type":"list-like","open":"","close":"","separator":"</pre><pre>","items":[{"type":"html","content":"<span class='clj-unkown'>#object[java.awt.image.BufferedImage 0x1cd4a633 &quot;BufferedImage@1cd4a633: type = 2 DirectColorModel: rmask=ff0000 gmask=ff00 bmask=ff amask=ff000000 IntegerInterleavedRaster: width = 32 height = 32 #Bands = 4 xOff = 0 yOff = 0 dataOffset[0] 0&quot;]</span>","value":"#object[java.awt.image.BufferedImage 0x1cd4a633 \"BufferedImage@1cd4a633: type = 2 DirectColorModel: rmask=ff0000 gmask=ff00 bmask=ff amask=ff000000 IntegerInterleavedRaster: width = 32 height = 32 #Bands = 4 xOff = 0 yOff = 0 dataOffset[0] 0\"]"},{"type":"html","content":"<span class='clj-unkown'>#object[javax.swing.JFrame 0xe792705 &quot;javax.swing.JFrame[frame0,0,0,136x132,layout=java.awt.BorderLayout,title=Imagez Frame,resizable,normal,defaultCloseOperation=DISPOSE_ON_CLOSE,rootPane=javax.swing.JRootPane[,8,31,120x93,layout=javax.swing.JRootPane$RootLayout,alignmentX=0.0,alignmentY=0.0,border=,flags=16777673,maximumSize=,minimumSize=,preferredSize=],rootPaneCheckingEnabled=true]&quot;]</span>","value":"#object[javax.swing.JFrame 0xe792705 \"javax.swing.JFrame[frame0,0,0,136x132,layout=java.awt.BorderLayout,title=Imagez Frame,resizable,normal,defaultCloseOperation=DISPOSE_ON_CLOSE,rootPane=javax.swing.JRootPane[,8,31,120x93,layout=javax.swing.JRootPane$RootLayout,alignmentX=0.0,alignmentY=0.0,border=,flags=16777673,maximumSize=,minimumSize=,preferredSize=],rootPaneCheckingEnabled=true]\"]"}],"value":"[#object[java.awt.image.BufferedImage 0x1cd4a633 \"BufferedImage@1cd4a633: type = 2 DirectColorModel: rmask=ff0000 gmask=ff00 bmask=ff amask=ff000000 IntegerInterleavedRaster: width = 32 height = 32 #Bands = 4 xOff = 0 yOff = 0 dataOffset[0] 0\"],#object[javax.swing.JFrame 0xe792705 \"javax.swing.JFrame[frame0,0,0,136x132,layout=java.awt.BorderLayout,title=Imagez Frame,resizable,normal,defaultCloseOperation=DISPOSE_ON_CLOSE,rootPane=javax.swing.JRootPane[,8,31,120x93,layout=javax.swing.JRootPane$RootLayout,alignmentX=0.0,alignmentY=0.0,border=,flags=16777673,maximumSize=,minimumSize=,preferredSize=],rootPaneCheckingEnabled=true]\"]]"}
-;; <=
+
+;; @@
+(def f (java.io.File. "data/cifar-10-batches-bin/data_batch_1.bin"))
+
+(def st (byte-streams/to-input-stream f))
+
+(def ch (take 1 (byte-streams/convert st (byte-streams/seq-of java.nio.ByteBuffer) {:chunk-size 1024})))
+
+(byte-streams/print-bytes ch)
+;; @@
 
 ;; @@
 
