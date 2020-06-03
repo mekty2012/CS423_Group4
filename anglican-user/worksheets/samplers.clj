5cc75b544a4885c590f8a777d67211f35a1725b9
Leesisi123 <56512187+Leesisi123@users.noreply.github.com>
2020-06-03 23:49:20 +0900

    Added test
    Added some tests for verification and realized stupidity of ourselves.

--- anglican-user/worksheets/samplers.clj
+++ anglican-user/worksheets/samplers.clj
@@ -75,14 +75,24 @@
 ;; @@
 
 ;; @@
+(defquery test-pi-sampler [n alpha]
+  (pi-sampler {:n n :alpha alpha})
+  )
+
+(def sample-test-pi)
+;; @@
+
+;; @@
 ; Since kernel here, is similar to CNN's single layer, I suggest random initialization method
 ; of CNN layer as probabilistic model. Note that this is not that reasonable. 
 ; The derivation of CNN layer initialization is based on signal preservation, where probabilistic programming
 ; does not contain any kind of signal propagation.
 (defn xavier [n] (sqrt (/ 6 (+ (* n n) 1))))
 
+(with-primitive-procedures [xavier]
 (defm kernel-sampler [n]
   (repeatedly n (fn [] (repeatedly n (fn [] (sample (uniform-continuous (- (xavier n)) (xavier n)))))))
+  )
   )
 ;; @@
 
@@ -96,9 +106,9 @@
         factor-mu (:factor-mu hyperparameters)
         factor-sigma (:factor-sigma hyperparameters)
         num_cluster (cluster-num-sampler lambda)
-        pi (pi-sampler num_cluster alpha)
-        mu_vec (repeatedly num_cluster (fn [] (mu-sampler n mu-mu mu-sigma)))
-        factor_vec (repeatedly num_cluster (fn [] (factor-sampler n factor-mu factor-sigma)))
+        pi (pi-sampler {:n num_cluster :alpha alpha})
+        mu_vec (repeatedly num_cluster (fn [] (mu-sampler {:n n :mu-mu mu-mu :mu-sigma mu-sigma})))
+        factor_vec (repeatedly num_cluster (fn [] (factor-sampler {:n n :factor-mu factor-mu :factor-sigma factor-sigma})))
         kernel_vec (repeatedly num_cluster (fn [] (kernel-sampler n)))]
     {:num_cluster num_cluster
      :pi pi
@@ -110,8 +120,6 @@
 ;; @@
 
 ;; @@
-;n p  lambda alpha mu-mu mu-sigma factor-mu factor-sigma
-
 (defm hierarchical-moe-sampler [hyperparameters]
   (let [n (:n hyperparameters)
         p (:p hyperparameters)
@@ -137,7 +145,7 @@
         
         child_vec (map (fn [i] 
                          (if (= i 1) 
-                           (hierarchical_moe_sampler hyperparameters)
+                           (hierarchical-moe-sampler hyperparameters)
                            (kernel-sampler n)
                            )) ischild_vec)]
  
@@ -152,23 +160,33 @@
 ;; @@
 
 ;; @@
-(defquery test-hierarchical-moe-sampler [n p lambda alpha mu-mu mu-sigma factor-mu factor-sigma]
-  (let [x (hierarchical-moe-sampler 
-            {:n n
-             :p p
-             :lambda lambda
-             :alpha alpha
-             :mu-mu mu-mu
-             :mu-sigma mu-sigma
-             :factor-mu factor-mu
-             :factor-sigma factor-sigma})]
-    {:x x}
-    )
+(defquery test-single-moe-sampler [n lambda alpha mu-mu mu-sigma factor-mu factor-sigma]
+	(single-moe-sampler {:n n :lambda lambda :alpha alpha :mu-mu mu-mu :mu-sigma mu-sigma :factor-mu factor-mu :factor-sigma factor-sigma})
   )
 ;; @@
 
 ;; @@
-(def sample (doquery :lmh test-hierarchical-moe-sampler 5 0.8 3 1.5 0 1 1 1))
+(def sample-sing (doquery :lmh test-single-moe-sampler [5 3 1.5 0 1 1 1]))
+
+(def results-sing (take 10 sample-sing))
+
+
+(print results-sing)
+;; @@
+
+;; @@
+(defquery test-hierarchical-moe-sampler [n p lambda alpha mu-mu mu-sigma factor-mu factor-sigma]
+  (hierarchical-moe-sampler {:n n :p p :lambda lambda :alpha alpha :mu-mu mu-mu :mu-sigma mu-sigma :factor-mu factor-mu :factor-sigma factor-sigma})
+  )
+;; @@
+
+;; @@
+(def sample-hier (doquery :lmh test-hierarchical-moe-sampler [5 0.5 3 1.5 0 1 1 1]))
+
+(def results-hier (take 10 sample-hier))
+
+
+(print results-hier)
 ;; @@
 
 ;; @@
