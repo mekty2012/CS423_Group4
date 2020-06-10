5dc44c7f0a2428a72e623c78bf056c34eb471731
mekty2012 <41375658+mekty2012@users.noreply.github.com>
2020-06-10 18:40:57 +0900

    Create train.clj

---
+++ anglican-user/src/moe/train.clj
@@ -0,0 +1,98 @@
+;; gorilla-repl.fileformat = 1
+
+;; **
+;;; # Gorilla REPL
+;;; 
+;;; Welcome to gorilla :-)
+;;; 
+;;; Shift + enter evaluates code. Hit alt+g twice in quick succession or click the menu icon (upper-right corner) for more commands ...
+;;; 
+;;; It's a good habit to run each worksheet in its own namespace: feel free to use the declaration we've provided below if you'd like.
+;; **
+
+;; @@
+(ns moe.train
+  (:require [gorilla-plot.core]
+            [clojure.core.matrix
+             :refer [matrix diagonal-matrix to-nested-vectors div ecount add]]
+            [clojure.core.matrix.operators]
+            [clojure.core.matrix.linear]
+            [moe.gaussian :refer :all]
+            [moe.batchreader :refer :all]
+            [moe.preprocess :refer :all]
+            [moe.samplers :refer :all]
+            [moe.moe :refer :all]
+            ))
+(use 'nstools.ns)
+(require 'mikera.image.core)
+(require 'mikera.image.colours)
+(require 'byte-streams)
+(ns+ template
+  (:like anglican-user.worksheet))
+;; @@
+
+;; @@
+(def single-hyperparameter 
+  {:n 49
+   :lambda 5
+   :alpha 1
+   :mu-mu 0
+   :mu-sigma 1
+   :factor-mu 0
+   :factor-sigma 1
+   :is-single true
+   :auto-tune false
+   :feeder "" ; best, prob, weight
+    })
+
+; p should be lesser then 1/lambda. 
+(def hierarchical-hyperparameter
+  {:n 49
+   :lambda 5
+   :p 0.01
+   :alpha 1
+   :mu-mu 0
+   :mu-sigma 1
+   :factor-mu 0
+   :factor-sigma 1
+   :is-single false
+   :auto-tune false
+   :feeder ""})
+
+(def autotune-single-hyperparameter
+  {:n 49
+   :lambda-tune 0.2
+   :alpha-tune 1
+   :mu-mu-tune 1
+   :mu-sigma-tune-a 5
+   :mu-sigma-tune-b 1
+   :factor-mu-tune 1
+   :factor-sigma-tune-a 5
+   :factor-sigma-tune-b 1
+   :is-single true
+   :auto-tune true
+   :feeder ""
+   })
+
+(def autotune-hierarchical-hyperparameter
+  {:n 49
+   :lambda-tune 0.2
+   :tune-p-a 2
+   :tune-p-b 2
+   :alpha-tune 1
+   :mu-mu-tune 1
+   :mu-sigma-tune-a 5
+   :mu-sigma-tune-b 1
+   :factor-mu-tune 1
+   :factor-sigma-tune-a 5
+   :factor-sigma-tune-b 1
+   :is-single false
+   :auto-tune true
+   :feeder ""
+   }
+  )
+;; @@
+
+;; @@
+
+;; @@
